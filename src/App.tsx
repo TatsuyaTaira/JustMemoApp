183535f34dd3e75d7bece7a15056a424c478acde
TatsuyaTaira <longye.jap@gmail.com>
2021-12-12 07:06:19 +0900

    ナビゲーションバーの追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { Row } from './compornents/Row';
 import './App.css';
 import { requests } from './request';
 import { Banner } from './compornents/Banner';
+import { Nav } from './compornents/Nav';
 
 function App() {
   return (
     <div className='App'>
+      <Nav />
       <Banner />
       <h3> 　もう寝たい</h3>
       <Row title='Netflix Originals' fetchUrl={requests.feachNetflixOriginals} isLargeRow />
       <Row title='Top Rated' fetchUrl={requests.feactTopRated} />
-      <Row title='Action Movies' fetchUrl={requests.feactActionMovies} />
       <Row title='Comedy Movies' fetchUrl={requests.feactComedyMovies} />
-      <Row title='Horror Movies' fetchUrl={requests.feactHorrorMovies} />
       <Row title='Romance Movies' fetchUrl={requests.feactRomanceMovies} />
       <Row title='DOcumentaries' fetchUrl={requests.feactDocumentMovies} />
     </div>
